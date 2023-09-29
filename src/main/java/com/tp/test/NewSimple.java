a2bed35136a34361cde43423836fdec01980651a
nate <nate>
2023-09-28 23:30:44 -0700

    add hi

--- src/main/java/com/tp/test/NewSimple.java
+++ src/main/java/com/tp/test/NewSimple.java
@@ -2,6 +2,9 @@ package com.tp.test;
 
 public class NewSimple {
     public static void main(String [] args) {
-        
+        System.out.println("hi");
+        System.out.println("hello");
+        System.out.println("end");
+
     }
 }
