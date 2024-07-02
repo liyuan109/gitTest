9316b9a8cfbcfae0960df81f9d4aec2d510c1457
Nate <lyuan109@gmail.com>
2024-07-02 10:31:49 -0700

    end again

--- src/main/java/com/tp/test/NewSimple.java
+++ src/main/java/com/tp/test/NewSimple.java
@@ -5,6 +5,6 @@ public class NewSimple {
         System.out.println("hi");
         System.out.println("hello");
         System.out.println("end");
-
+        System.out.println("endagain");
     }
 }
