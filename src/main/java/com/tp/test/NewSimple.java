71e605160ae6073665048bd17a7bd76778d36fc2
Nate <lyuan109@gmail.com>
2024-08-27 23:14:35 -0700

    enter 5 changes

--- src/main/java/com/tp/test/NewSimple.java
+++ src/main/java/com/tp/test/NewSimple.java
@@ -10,5 +10,7 @@ public class NewSimple {
         System.out.println("enter_2 changes");
         System.out.println("enter_4 changes");
         System.out.println("enter_4 changes 2");
+        System.out.println("enter_5 changes");
+
     }
 }
